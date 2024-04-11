571bd4531e91cbf7977f387173858230b44f8ea0
Alex <giraldojhong4@gmail.com>
2024-04-11 00:33:40 -0500

    Mejoras de editar contactos

--- LIN.Contacts/LIN.Contacts.Client/Program.cs
+++ LIN.Contacts/LIN.Contacts.Client/Program.cs
@@ -5,6 +5,7 @@ global using LIN.Types.Responses;
 global using Microsoft.JSInterop;
 global using LIN.Types.Contacts.Models;
 global using Microsoft.AspNetCore.Components;
+global using LIN.Access.Contacts;
 using Microsoft.AspNetCore.Components.WebAssembly.Hosting;
 
 var builder = WebAssemblyHostBuilder.CreateDefault(args);
